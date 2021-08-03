ba0eab4540ffc9fe2e3e143a41d590480ad8b18f
mattia biscontini <mattiabiscontini@gmail.com>
2021-08-03 08:07:06 +0100

    Navbar and nav-links created, once logged in it will show

---
+++ src/components/navbar/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom';
+
+const Navbar = () => {
+
+    return (
+        <div className="navbar">
+            <NavLink to ="">Home</NavLink>
+            <NavLink to ="">Movies</NavLink>
+            <NavLink to ="">Series</NavLink>
+            <NavLink to ="">Watchlist</NavLink>
+            <NavLink to ="">Account</NavLink>
+        </div>
+    )
+}
+
+export default Navbar;
---
+++ src/utils/index.js
@@ -0,0 +1,26 @@
+export const fetchUsers = async (e, email, username, pass, setUser) => {
+    e.preventDefault();
+    try {
+        let response;
+        if (email) {
+            response = await fetch(`${process.env.REACT_APP_REST_API}users`, {
+                method: 'POST',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify({
+                    email: email,
+                    username: username,
+                    password: pass
+                })
+            })
+        } else {
+            response = await fetch(`${process.env.REACT_APP_REST_API}users/${username}`)
+        }
+        const data = await response.json();
+        setUser(data.user.username)
+    
+    } catch (error) {
+        console.log(error);
+    }
+}
+   
+
